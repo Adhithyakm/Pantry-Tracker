f3718a6067ab8abdecd5e872d65ee68cc594e225
Adhithya <145639316+Adhithyakm@users.noreply.github.com>
2024-08-04 17:08:32 +0530

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,8 +19,16 @@ const firebaseConfig = {
   measurementId: "G-J676SEQC3Z"
 };
 
+let firebaseApp;
+let auth;
+
+if (typeof window !== 'undefined') {
+  firebaseApp = initializeApp(firebaseConfig);
+  auth = getAuth(firebaseApp);
+}
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
-export { firestore };
+export { firestore ,firebaseApp, auth};
